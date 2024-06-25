0621af4e7d13ece1ac28540a82a427a0034691db
Erick - coneng <erickkbonfim@gmail.com>
2024-06-25 10:27:52 -0300

    Implementação de dados gerais, e pendencias de iptu

--- report/src/Components/PendenciasIptu/PendenciasIptu.jsx
+++ report/src/Components/PendenciasIptu/PendenciasIptu.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function PendenciasIptu() {
+export default function PendenciasIptu({iptu}) {
   return (
-    <div>PendenciasIptu</div>
+    <div className='PendenciasIptu'>
+      <h3> Pendências de IPTU </h3>
+        {iptu.map((item, index) => (
+          <div className='HollPendencias' key={index}>
+            <li> { item } </li>
+          </div>
+        ))}
+    </div>
   )
 }
